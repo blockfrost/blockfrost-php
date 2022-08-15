59a14601b127f5661be46247597cd1e979454a58
NeilDWood <NeilDWood@hotmail.com>
2022-08-15 20:52:58 +0100

    updated test for workflow10

--- test/AccountServiceTest.php
+++ test/AccountServiceTest.php
@@ -1,6 +1,6 @@
 <?php
 
-//require_once __DIR__ . '/../examples/vendor/autoload.php';
+require_once __DIR__ . '/../examples/vendor/autoload.php';
 
 use PHPUnit\Framework\TestCase;
 use Blockfrost\Account\AccountsService;
