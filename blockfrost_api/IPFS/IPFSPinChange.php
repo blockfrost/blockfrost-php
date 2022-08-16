7093ccdc17388787556b0a8e3f920c6275851560
NeilDWood <NeilDWood@hotmail.com>
2022-08-16 15:47:41 +0100

    updated test for workflow24

---
+++ blockfrost_api/IPFS/IPFSPinChange.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace Blockfrost\IPFS;
+
+class IPFSPinChange
+{
+    public $ipfs_hash;      // : "QmZbHqiCxKEVX7QfijzJTkZiSi3WEVTcvANgNAWzDYgZDr",
+    public $state;          // "queued"
+}
+
+?>
